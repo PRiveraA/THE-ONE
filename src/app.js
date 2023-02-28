0a5a4b53910b7622b4d746a496344c5b5b674d02
PRiveraA <priveraa94@gmail.com>
2023-02-28 01:24:10 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomVal = () => {
+  let val = ["2", "3", "4", "5", "6", "7", "8", "9", "K", "Q", "A"];
+  let i = Math.floor(Math.random() * 10);
+  return val[i];
+};
+
+let randomSuit = () => {
+  let suit = ["♦", "♠", "♥", "♣"];
+  let iSuit = Math.floor(Math.random() * 3);
+  return suit[iSuit];
+};
+
+const suit = randomSuit();
+window.onload = () => {
+  document.querySelector(".value").textContent = randomVal();
+  document.querySelectorAll(".suit").forEach(elem => {
+    elem.textContent = suit;
+    if (elem.textContent === "♦" || elem.textContent === "♥") {
+      elem.classList.add("red");
+    }
+  });
 };
